186f89ea61b5d2742370b5b6d8d9f5bf8adea947
Andrew Huertas <ahuertas@student.neumont.edu>
2014-08-08 05:06:13 +0000

    git-svn-id: https://svn.neumont.edu:8443/svn/ahuertas_richanimation/trunk@8 945d50e8-190b-8c40-a43b-fad4c07808d2

--- Lab 1 - Game Template/Lab 1 - Game Template/game.js
+++ Lab 1 - Game Template/Lab 1 - Game Template/game.js
@@ -1,70 +1,188 @@
-
-var manifest =
+var MODE_TITLE = 0;
+var MODE_INSTRUCTIONS = 1;
+var mode = MODE_TITLE;
+var titleManifest =
 [
-	{src:"title.png", id:"title"},
-	{src:"background.png", id:"background"},
-	{src:"instructions.png", id:"instructions"},
-	{src:"gameover.png", id:"gameover"},
-	{src:"levelsign.png", id:"levelsign"}
+	{ src: "title.jpg", id: "title" },
+	{ src: "titleButtons.jpg", id: "titleButtons" }
 ];
+
+var instructionManifest =
+	[
+			{ src: "instructions.png", id: "instructions" }
+	];
+
+var gameManifest =
+	[
+	{ src: "background.png", id: "background" },
+	{ src: "gameover.png", id: "gameover" },
+	{ src: "levelsign.png", id: "levelsign" }
+	];
 var FPS = 30;
 var stage;
-var canvas;
-var queue;
-var titleScreen;
-var backgroundScreen;
-var instructionScreen;
-var gameoverScreen;
-var levelFrame;
+var titleQueue, titleScreen, playButton;
+
+var instructionQueue, instructionScreen;
+
+var gameQueue, backgroundScreen, gameoverScreen, levelFrame;
 
 function setUpCanvas()
 {
-	canvas = document.getElementById("game");
+	var canvas = document.getElementById( "game" );
 	canvas.width = 800;
 	canvas.height = 600;
-	stage = new createjs.Stage(canvas);
+	stage = new createjs.Stage( canvas );
+	stage.enableMouseOver();
 }
 
-function loadFiles()
+function startLoad()
 {
-	queue = new createjs.LoadQueue(true, "assets/images/");
-	queue.on("complete", loadComplete, this);
-	queue.loadManifest( manifest );
+	titleQueue = new createjs.LoadQueue( true, "assets/images/" );
+	titleQueue.on( "complete", titleLoaded, this );
+	titleQueue.loadManifest( titleManifest );
 }
 
-function loadComplete()
+function titleLoaded()
 {
-	titleScreen = new createjs.Bitmap(queue.getResult("title"));
-    backgroundScreen = new createjs.Bitmap(queue.getResult("background"));
-    instructionScreen = new createjs.Bitmap(queue.getResult("instructions"));
-    gameoverScreen = new createjs.Bitmap(queue.getResult("gameover"));
-    levelFrame = new createjs.Bitmap(queue.getResult("levelsign"));
+	titleScreen = new createjs.Bitmap( titleQueue.getResult( "title" ) );
+
+	var playButtonSheet = new createjs.SpriteSheet
+		(
+	{
+		images: [titleQueue.getResult( "titleButtons" )],
+		frames:
+			{
+				regX: 150 / 2,
+				regY: 30 / 2,
+				width: 150,
+				height: 30,
+				count: 6
+			},
+		animations:
+			{
+				Neutral: [0, 0],
+				Hover: [1, 1],
+				Click: [2, 2]
+			}
+	}
+		);
+
+	playButton = new createjs.Sprite( playButtonSheet );
+
+	var instructionsButtonSheet = new createjs.SpriteSheet
+	(
+{
+	images: [titleQueue.getResult( "titleButtons" )],
+	frames:
+		{
+			regX: 150 / 2,
+			regY: 30 / 2,
+			width: 150,
+			height: 30,
+			count: 6
+		},
+	animations:
+		{
+			Neutral: [3, 3],
+			Hover: [4, 4],
+			Click: [5, 5]
+		}
+}
+	);
+
+	instructionsButton = new createjs.Sprite( instructionsButtonSheet );
+
+	instructionQueue = new createjs.LoadQueue( true, "assets/images/" );
+	instructionQueue.on( "complete", instructionsLoaded, this );
+	instructionQueue.loadManifest( instructionManifest );
 }
 
+function instructionsLoaded()
+{
+	instructionScreen = new createjs.Bitmap( instructionQueue.getResult( "instructions" ) );
+
+	gameQueue = new createjs.LoadQueue( true, "assets/images/" );
+	gameQueue.on( "complete", gameLoaded, this );
+	gameQueue.loadManifest( gameManifest );
+}
+
+function gameLoaded()
+{
+	backgroundScreen = new createjs.Bitmap( gameQueue.getResult( "background" ) );
+	gameoverScreen = new createjs.Bitmap( gameQueue.getResult( "gameover" ) );
+	levelFrame = new createjs.Bitmap( gameQueue.getResult( "levelsign" ) );
+}
+
+var titleInitialized = false;
+function titleInit()
+{
+	stage.addChild( titleScreen );
+	stage.addChild( playButton );
+	stage.addChild( instructionsButton );
+	playButton.x = stage.canvas.width - ( 150 * 1.5 );
+	playButton.y = stage.canvas.height - ( 30 / 2 );
+	instructionsButton.x = stage.canvas.width - ( 150 * 0.5 );
+	instructionsButton.y = stage.canvas.height - ( 30 / 2 );
+	playButton.gotoAndPlay( "Neutral" );
+	playButton.on( "mouseover", function playHover( evt ) { playButton.gotoAndPlay( "Neutral" ); }, this );
+	playButton.on( "mouseout", function playHover( evt ) { playButton.gotoAndPlay( "Hover" ); }, this );
+	playButton.on( "click", function playHover( evt ) { playButton.gotoAndPlay( "Click" ); }, this );
+
+	instructionsButton.gotoAndPlay( "Neutral" );
+	instructionsButton.on( "mouseover", function playHover( evt ) { instructionsButton.gotoAndPlay( "Neutral" ); }, this );
+	instructionsButton.on( "mouseout", function playHover( evt ) { instructionsButton.gotoAndPlay( "Hover" ); }, this );
+	instructionsButton.on( "click", function playHover( evt ) { instructionsButton.gotoAndPlay( "Click" ); }, this );
+	titleInitialized = true;
+}
+function titleDelete()
+{
+	stage.removeAllChildren();
+	titleInitialized = false;
+}
+function removeAll()
+{
+	if ( titleInitialized )
+	{
+		titleDelete();
+	}
+}
 function titleUpdate()
 {
-	
+	if ( !titleInitialized )
+	{
+		removeAll();
+		titleInit();
+	}
 }
+
 
 function loop()
 {
-	titleUpdate();
+	switch ( mode )
+	{
+		case ( MODE_TITLE ):
+			{
+				if(titleQueue.loaded)
+				titleUpdate();
+				break;
+			}
+	}
 	stage.update();
 }
 
 function main()
 {
-	loadFiles();
 	setUpCanvas();
-	createjs.Ticker.addEventListener("tick", loop);
-	createjs.Ticker.setFPS(FPS);
+	startLoad();
+	createjs.Ticker.addEventListener( "tick", loop );
+	createjs.Ticker.setFPS( FPS );
 }
 
-if( !!(window.addEventListener))
+if ( !!( window.addEventListener ) )
 {
-    window.addEventListener ("DOMContentLoaded", main);
+	window.addEventListener( "DOMContentLoaded", main );
 }
 else
 { //MSIE
-    window.attachEvent("onload", main);
+	window.attachEvent( "onload", main );
 }
